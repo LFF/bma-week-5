4793379fa01a7ecff58122832855019d5322a1a4
luisfonseca <luisfonseca@192.168.1.208>
2012-10-07 13:38:04 -0400

    add Assigment java after .ignore add

--- bma-week-5/src/Assigment.java
+++ bma-week-5/src/Assigment.java
@@ -2,11 +2,67 @@
 public class Assigment {
 
 	/**
+	 * 
+	 * test assigment file after .gitignore add
+	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[]  args) {
+		
+			
+		 
+		   int  index  =   0;
+		      
+			
+			  String  values []  = {"a", "b", "c", "d", "e"};
+			       		
+ 			
+	         getElement(values, index);
+			
+		
+		
 	}
+	
+	
+	
+	 static String getElement(String values[]  , int  index) {
+		 
+		 
+		  if  ( index < 0  ||  index  > values.length ) 
+			  
+			  
+			  
+		  {		System.out.println("bad index" + index);
+			  
+		 
+		           return null;  }
+		  
+		  
+		  else
+			  
+		  {
+		  
+		           
+		       	System.out.println("valid index"  + index);
+		           
+		
+		    return values[index];
+		 
+		 
+				
+			}
+			
+	 }
+			
+	
 
 }
+
+
+
+
+           class Task {  }
+
+
+
+
